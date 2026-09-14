3a4a6795f5fd72bde34e82e05a75b5fcc7854966
Amirhossein <iamhoss14@gmail.com>
2026-09-14 19:11:50 +0330

    Add project files

--- src/test.py
+++ src/test.py
@@ -3,3 +3,4 @@ url = "https://v6.exchangerate-api.com/v6/285dfe149039c038fec336df/latest/USD"
 response = requests.get(url)    
 response.json()
 #----------------------------------------------------------------------------------
+print(response.json())
